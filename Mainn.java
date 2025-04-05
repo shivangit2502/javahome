a134e16d139af1b9e10bcfdcd2140ee6b4380356
Shivangi Thapliyal <shivangithapliyal25@gmail.com>
2025-04-05 22:07:58 +0530

    Add files via upload

---
+++ Mainn.java
@@ -0,0 +1,9 @@
+
+import Balance.Account;
+
+public class Mainn {
+    public static void main(String[] args) {
+        Account myAccount = new Account(15000.0); // You can change the amount
+        myAccount.Display_Balance();
+    }
+}
